c18e8bfae5bd9f6feb346f8294404e4ee5ddc3dd
Ryan Lee <ryanlee91@gmail.com>
2017-11-11 14:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Write your code in this file!
 function scuberGreetingForFeet(){
-  if  {
-    
+  if (ride <=400) {
+    return 'You can get a free sample!'
+  } else if (ride < 2500) {
+      return 'You owe $30'
+    else if (ride > 2500) {
+      return 'You cannot ride.'
+    }
   }
 }
