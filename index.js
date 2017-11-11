7fa5978f6435b6e63da4f2139400e16d1374d701
Ryan Lee <ryanlee91@gmail.com>
2017-11-11 14:38:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,11 @@ function scuberGreetingForFeet(feet){
       return 'No can do.';
   }
 }
-
-
+function ternaryCheckCity(city){
+  if (city = 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else if (city != 'NYC') {
+      return 'No go.'
+  }
+}
