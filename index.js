892bb5f000e6b0eac4a7e8ec64d357db20f7370c
Ryan Lee <ryanlee91@gmail.com>
2017-11-11 14:34:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your code in this file!
 function scuberGreetingForFeet(){
   if (feet <=400) {
-    return 'This one is on me!'
+    return 'This one is on me!';
   } else if (1999 < feet && feet < 2500) {
-      return 'I will take you for $30'
-    else if (feet > 2500) {
-      return 'You cannot ride.'
-    }
+      return 'I will take you for $30';
+  }  else if (feet > 2500) {
+      return 'You cannot ride.';
   }
 }
+
